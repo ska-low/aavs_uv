c5f066bdf9b4fb2637cc19fa04494dc7ce3550f3
Danny Price <danny.price@skao.int>
2023-10-05 21:41:49 +0800

    Updated with reading AAVS3 MCCS file

--- aavs_uv.py
+++ aavs_uv.py
@@ -25,6 +25,31 @@ def load_yaml(filename: str) -> dict:
     return d
 
 
+def station_location_from_platform_yaml(fn_yaml: str) -> (EarthLocation, pd.DataFrame):
+    """ Load station location from AAVS3 MCCS yaml config 
+
+    Args:
+        fn_yaml (str): Filename path to yaml config
+
+    Returns:
+        (earth_loc, ant_enu): astropy EarthLocation and antenna ENU locations in m
+    """
+    d =load_yaml(fn_yaml)
+    d_station = d['platform']['array']['station_clusters']['a1']['stations']['1']
+
+    # Generate pandas dataframe of antenna positions
+    d_ant = d_station['antennas']
+    ant_enu = [[int(k), *list(d_ant[k]['location_offset'].values())] for k in d_ant.keys()]
+    ant_enu = pd.DataFrame(ant_enu, columns=('name', 'E', 'N', 'U'))
+
+    # Generate array central reference position
+    # NOTE: Currently using WGS84 instead of GDA2020
+    loc = d_station['reference']
+    earth_loc = EarthLocation.from_geodetic(loc['longitude'], loc['latitude'], loc['ellipsoidal_height'])
+
+    return  earth_loc, ant_enu
+    
+
 def get_hdf5_metadata(filename: str) -> dict:
     """ Extract metadata from HDF5 and perform checks """
     with h5py.File(filename, mode='r') as datafile:
