4d68fdcfecbf3741d3fca4e92819374a57efe846
Danny Price <danny.price@skao.int>
2023-11-28 13:29:53 +0000

    Added uv5 test script

---
+++ tests/test_uv5.py
@@ -0,0 +1,12 @@
+from aavs_uv.io import hdf5_to_uv, uv5_to_uv, uv_to_uv5
+
+def test_roundtrip():
+    fn = '../example-data/aavs2_2x500ms/correlation_burst_204_20230927_35116_0.hdf5'
+    uv = hdf5_to_uv(fn, telescope_name='aavs2')
+
+    uv_to_uv5(uv, 'test.h5')
+
+    uv2 = uv5_to_uv('test.h5')
+
+if __name__ == "__main__":
+    test_roundtrip()
