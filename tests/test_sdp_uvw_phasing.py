1928ea748465d20bbbf050c0b069e0eda63b5224
Danny Price <danny.price@skao.int>
2024-01-29 15:44:32 +0800

    Updated SDP UVW tests for new ska-sdp-datamodels (with bugfix)

--- tests/test_sdp_uvw_phasing.py
+++ tests/test_sdp_uvw_phasing.py
@@ -18,10 +18,8 @@ def test_phasing():
     try:   
         # Read in test data, converting into a pyuvdata.UVData and sdp Visibility 
         test_data_fn = 'test-data/aavs2_2x500ms/correlation_burst_204_20230927_35116_0.hdf5'
-        uvd  = hdf5_to_pyuvdata(test_data_fn, telescope_name='aavs2', phase_to_t0=False)
         uvdp = hdf5_to_pyuvdata(test_data_fn, telescope_name='aavs2', phase_to_t0=True)
         sdp = hdf5_to_sdp_vis(test_data_fn, telescope_name='aavs2')
-        uv  = hdf5_to_uvx(test_data_fn, telescope_name='aavs2')
 
         # Write out the UVData as a MS, and write out the SDP visibilities as a H5 file
         uvdp.write_ms('test.ms')
@@ -32,20 +30,17 @@ def test_phasing():
             sdp_sdp = convert_hdf_to_visibility(h['Visibility0'])
         sdp_ms = create_visibility_from_ms('test.ms')[0]
 
-        # WAR - Fix YY and XY swap 
-        vis_sdp_corr = np.zeros_like(sdp_ms.vis.values)
-        vis_sdp_corr[..., 0] = sdp_ms.vis[..., 0] # XX
-        vis_sdp_corr[..., 1] = sdp_ms.vis[..., 2] # XY
-        vis_sdp_corr[..., 2] = sdp_ms.vis[..., 3] # YX
-        vis_sdp_corr[..., 3] = sdp_ms.vis[..., 1] # YY
-
-        sdp_ms.vis.values = vis_sdp_corr
-
         # Check that data are complex conjugate of each other
-        print(sdp_ms.vis.values[0])
-        print(sdp_sdp.vis.values[0])
-        assert np.allclose(sdp_ms.vis.values, sdp_sdp.vis.values)
+        print("---")
+        print(sdp.vis.values[0,1])
+        print("---")
+        print(sdp_ms.vis.values[0, 1])
+        print("---")
+        print(sdp_sdp.vis.values[0,1])
+
         assert np.allclose(sdp_ms.uvw, sdp_sdp.uvw, atol=0.5e-7)
+        assert np.allclose(sdp_ms.vis.values, sdp_sdp.vis.values)
+        
     finally:
         if os.path.exists('test.ms'):
             os.system('rm -rf test.ms')
