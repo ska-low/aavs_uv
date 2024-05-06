0490aa2cdade559d1914d30bf5456acce45a940e
Danny Price <danny.price@skao.int>
2024-05-06 16:41:47 +0800

    Adding test_antenna_cal.py

---
+++ tests/test_antenna_cal.py
@@ -0,0 +1,25 @@
+import numpy as np
+from astropy.units import Quantity
+from aavs_uv.datamodel.cal import (
+    create_antenna_cal, create_antenna_flags, create_uvx_antenna_cal
+)
+
+def test_antenna_cal():
+    N_ant = 256
+    N_freq = 1
+    N_pol = 4
+
+    cal = np.zeros((N_freq, N_ant, N_pol), dtype='complex64')
+    flags = np.zeros_like(cal, dtype='bool')
+
+    a = np.arange(N_ant)
+    f = Quantity(np.arange(N_freq), unit='Hz')
+    p = np.array(('xx', 'xy', 'yx', 'yy'))
+
+    antenna_cal = create_antenna_cal(cal, f, a, p)
+    antenna_flags = create_antenna_flags(cal, f, a, p)
+
+    cal = create_uvx_antenna_cal('AAVS3', 'manual', cal, flags, f, a, p, {'history':  'yo'})
+
+if __name__ == "__main__":
+    test_antenna_cal()
